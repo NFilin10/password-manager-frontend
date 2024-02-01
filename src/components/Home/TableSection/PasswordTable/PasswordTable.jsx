1c678dcbddaa22f17cd9340319440851022992b6
Nikita Filin <nikita10filin@gmail.com>
2024-02-01 19:28:11 +0200

    password ferching implemented

--- src/components/Home/TableSection/PasswordTable/PasswordTable.jsx
+++ src/components/Home/TableSection/PasswordTable/PasswordTable.jsx
@@ -36,83 +36,100 @@ const PasswordTable = () => {
     }, []);
 
 
-    return (
-        <div className={Styles.passwordTableWrapper}>
-            <table>
-                <thead>
-                <tr>
-                    <th>Website</th>
-                    <th>Categories</th>
-                    <th>Username</th>
-                    <th>Password</th>
-                    <th>Vulnerability score</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                    <td className={Styles.website}>
-                        <div className={Styles.websiteContainer}>
-                            <img className={Styles.icon} src={InstagramLogo} alt=""/>
-                            <div className={Styles.content}>
-                                <h5>Instagram</h5>
-                                <a href="http://instagram.com">http://instagram.com</a>
-                            </div>
-                        </div>
+
+        const [photos, setPhotos] = useState([]);
+        useEffect(() => {
+            fetch(`http://localhost:8080/`)
+                .then((res) => {
+                    return res.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setPhotos(data);
+                });
+        }, []);
 
 
-                    </td>
-                    <td className={Styles.categories}>
-                        <div className={Styles.categoriesWrapper}>
-                            <Category categoryName={{name:"Art"}}/>
-                            <Category categoryName={{name:"School"}}/>
-                            <Category categoryName={{name:"Art"}}/>
-                            <Category categoryName={{name:"Art"}}/>
-
-                        </div>
-                    </td>
-                    <td className={Styles.username}>
-                        <div className={Styles.usernameWrapper}>
-                            <p>username@usaer</p>
-                            <FontAwesomeIcon icon={faCopy} className={Styles.helper} />
-                        </div>
-
-                    </td>
-                    <td className={Styles.password}>
-                        <div className={Styles.passwordWrapper}>
-                            <p>*********</p>
-                            <FontAwesomeIcon icon={faEye} className={Styles.helper}/>
-                            <FontAwesomeIcon icon={faCopy} className={Styles.helper}/>
-                        </div>
-
-                    </td>
-                    <td className={Styles.vuln}>
-                        <div className={Styles.vulnContainer}>
-                            <ProgressBar
-                                percent={perc}
-                                fillColor={perc > 50 ? "green" : perc < 20 ? "red" : "orange"}
-                                width="50px"
-                                height="15px"
-                            />
-                            {perc}%
-                            <div className={Styles.options}>
-                                <span ref={imgRef} onClick={() => setOpen(!open)} className={Styles.more}><FontAwesomeIcon icon={faEllipsisVertical} /></span>
-                                {open && (
-                                    <div ref={menuRef} className={Styles.dropdown}>
-                                        <li>Edit</li>
-                                        <li>Delete</li>
+        return (
+            <div className={Styles.passwordTableWrapper}>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Website</th>
+                        <th>Categories</th>
+                        <th>Username</th>
+                        <th>Password</th>
+                        <th>Vulnerability score</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {photos.map((photo) => (
+                            <tr>
+                                <td className={Styles.website}>
+                                    <div className={Styles.websiteContainer}>
+                                        <img className={Styles.icon} src={InstagramLogo} alt=""/>
+                                        <div className={Styles.content}>
+                                            <h5>{photo.service_name}</h5>
+                                            <a href={photo.link}>{photo.link}</a>
+                                        </div>
                                     </div>
-                                )}
-                            </div>
+                                </td>
+
+                                <td className={Styles.categories}>
+                                    <div className={Styles.categoriesWrapper}>
+                                        <Category categoryName={{name:"Art"}}/>
+                                        <Category categoryName={{name:"School"}}/>
+                                        <Category categoryName={{name:"Art"}}/>
+                                        <Category categoryName={{name:"Art"}}/>
+                                    </div>
+                                </td>
+
+                                <td className={Styles.username}>
+                                    <div className={Styles.usernameWrapper}>
+                                        <p>{photo.login}</p>
+                                        <FontAwesomeIcon icon={faCopy} className={Styles.helper} />
+                                    </div>
+                                </td>
+
+                                <td className={Styles.password}>
+                                    <div className={Styles.passwordWrapper}>
+                                        <p>{photo.password}</p>
+                                        <FontAwesomeIcon icon={faEye} className={Styles.helper}/>
+                                        <FontAwesomeIcon icon={faCopy} className={Styles.helper}/>
+                                    </div>
+                                </td>
+
+                                <td className={Styles.vuln}>
+                                    <div className={Styles.vulnContainer}>
+                                        <ProgressBar
+                                            percent={perc}
+                                            fillColor={perc > 50 ? "green" : perc < 20 ? "red" : "orange"}
+                                            width="50px"
+                                            height="15px"
+                                        />
+                                        {perc}%
+                                        <div className={Styles.options}>
+                                            <span ref={imgRef} onClick={() => setOpen(!open)} className={Styles.more}><FontAwesomeIcon icon={faEllipsisVertical} /></span>
+                                            {open && (
+                                                <div ref={menuRef} className={Styles.dropdown}>
+                                                    <li>Edit</li>
+                                                    <li>Delete</li>
+                                                </div>
+                                            )}
+                                        </div>
 
 
-                        </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-        </div>
-    )
+                                    </div>
+                                </td>
+
+                            </tr>
+                        ))}
+
+                    </tbody>
+                </table>
+            </div>
+        )
+
 }
-
 export default PasswordTable
 
