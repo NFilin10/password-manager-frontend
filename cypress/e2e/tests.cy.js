404ccf0d95c0b935dda1bec607a5303dac0c1b93
Nikita Filin <nikita10filin@gmail.com>
2024-02-13 22:50:33 +0200

    Login tests implemented

---
+++ cypress/e2e/tests.cy.js
@@ -0,0 +1,55 @@
+describe('Login test', () => {
+  it('Correct login credentials', () => {
+    cy.visit('https://password-manager-frontend-ten.vercel.app/login')
+
+    cy.get('.LoginForm_input__C4YBF').eq(1).type("test@gmail.com")
+    cy.get('.LoginForm_input__C4YBF').eq(2).type("testpassword")
+
+    cy.intercept('POST', '/auth/login').as('loginRequest')
+
+    cy.get('.LoginForm_button__hHec2').eq(0).click()
+
+    cy.wait('@loginRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(200)
+      cy.wrap(interception.response.body).should('have.property', 'token');
+      cy.wrap(interception.response.body).should('have.property', 'user_id');
+    })
+  })
+
+  it('Wrong password', () => {
+    cy.visit('https://password-manager-frontend-ten.vercel.app/login')
+
+    cy.get('.LoginForm_input__C4YBF').eq(1).type("test@gmail.com")
+    cy.get('.LoginForm_input__C4YBF').eq(2).type("testpasswordwrong")
+
+    cy.intercept('POST', '/auth/login').as('loginRequest')
+
+    cy.get('.LoginForm_button__hHec2').eq(0).click()
+
+    cy.wait('@loginRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(401)
+      cy.wrap(interception.response.body).its('error').should('eq', 'Incorrect password');
+
+    })
+  })
+
+  it('Not registered user', () => {
+    cy.visit('https://password-manager-frontend-ten.vercel.app/login')
+
+    cy.get('.LoginForm_input__C4YBF').eq(1).type("test1@gmail.com")
+    cy.get('.LoginForm_input__C4YBF').eq(2).type("testpassword")
+
+    cy.intercept('POST', '/auth/login').as('loginRequest')
+
+    cy.get('.LoginForm_button__hHec2').eq(0).click()
+
+    cy.wait('@loginRequest').then((interception) => {
+      expect(interception.response.statusCode).to.eq(401)
+      cy.wrap(interception.response.body).its('error').should('eq', 'User is not registered');
+
+    })
+  })
+
+
+
+})
