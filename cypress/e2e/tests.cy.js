a552166a03e87ca344d522c1ab1d81aafab6387c
Nikita Filin <nikita10filin@gmail.com>
2024-02-13 23:30:40 +0200

    Logout test fixed

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,4 +1,4 @@
-/*
+
 describe('Login tests', () => {
     it('Correct login credentials', () => {
         cy.visit('https://password-manager-frontend-ten.vercel.app/login')
@@ -52,50 +52,50 @@ describe('Login tests', () => {
     })
 })
 
-*/
+
 
 describe('Signup tests', () => {
-    // it('Correct signup data', () => {
-    //     cy.visit('https://password-manager-frontend-ten.vercel.app/signup')
-    //     cy.get('.SignupForm_input__GWtGw').eq(1).type("TestName")
-    //     cy.get('.SignupForm_input__GWtGw').eq(2).type("TestSurname")
-    //     cy.get('.SignupForm_input__GWtGw').eq(3).type("test@mail.eu")
-    //     cy.get('.SignupForm_input__GWtGw').eq(4).type("testpassword")
-    //
-    //     cy.intercept('POST', '/auth/signup').as('signupRequest')
-    //
-    //
-    //     cy.get('.SignupForm_button__lbh0n').click()
-    //
-    //     cy.wait('@signupRequest').then((interception) => {
-    //         expect(interception.response.statusCode).to.eq(201)
-    //         cy.wrap(interception.response.body).should('have.property', 'token');
-    //
-    //     })
-    // })
+    it('Correct signup data', () => {
+        cy.visit('https://password-manager-frontend-ten.vercel.app/signup')
+        cy.get('.SignupForm_input__GWtGw').eq(1).type("TestName")
+        cy.get('.SignupForm_input__GWtGw').eq(2).type("TestSurname")
+        cy.get('.SignupForm_input__GWtGw').eq(3).type("test@mail.eu")
+        cy.get('.SignupForm_input__GWtGw').eq(4).type("testpassword")
+
+        cy.intercept('POST', '/auth/signup').as('signupRequest')
 
 
-    // it('Existing user', () => {
-    //     cy.visit('https://password-manager-frontend-ten.vercel.app/signup')
-    //     cy.get('.SignupForm_input__GWtGw').eq(1).type("TestName")
-    //     cy.get('.SignupForm_input__GWtGw').eq(2).type("TestSurname")
-    //     cy.get('.SignupForm_input__GWtGw').eq(3).type("test@mail.eu")
-    //     cy.get('.SignupForm_input__GWtGw').eq(4).type("testpassword")
-    //
-    //     cy.intercept('POST', '/auth/signup').as('signupRequest')
-    //
-    //
-    //     cy.get('.SignupForm_button__lbh0n').click()
-    //
-    //     cy.wait('@signupRequest').then((interception) => {
-    //         expect(interception.response.statusCode).to.eq(401)
-    //         cy.wrap(interception.response.body).its('error').should('eq', 'User is already registered');
-    //
-    //     })
-    //
-    //     cy.get('h3').should('contain', 'User is already registered')
-    //
-    // })
+        cy.get('.SignupForm_button__lbh0n').click()
+
+        cy.wait('@signupRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(201)
+            cy.wrap(interception.response.body).should('have.property', 'token');
+
+        })
+    })
+
+
+    it('Existing user', () => {
+        cy.visit('https://password-manager-frontend-ten.vercel.app/signup')
+        cy.get('.SignupForm_input__GWtGw').eq(1).type("TestName")
+        cy.get('.SignupForm_input__GWtGw').eq(2).type("TestSurname")
+        cy.get('.SignupForm_input__GWtGw').eq(3).type("test@mail.eu")
+        cy.get('.SignupForm_input__GWtGw').eq(4).type("testpassword")
+
+        cy.intercept('POST', '/auth/signup').as('signupRequest')
+
+
+        cy.get('.SignupForm_button__lbh0n').click()
+
+        cy.wait('@signupRequest').then((interception) => {
+            expect(interception.response.statusCode).to.eq(401)
+            cy.wrap(interception.response.body).its('error').should('eq', 'User is already registered');
+
+        })
+
+        cy.get('h3').should('contain', 'User is already registered')
+
+    })
 })
 
 
@@ -106,11 +106,7 @@ describe('Logout test', () => {
         cy.get('.LoginForm_input__C4YBF').eq(1).type("test@gmail.com")
         cy.get('.LoginForm_input__C4YBF').eq(2).type("testpassword")
 
-        // cy.intercept('POST', '/auth/login').as('loginRequest')
-
         cy.get('.LoginForm_button__hHec2').eq(0).click()
-
-
 
         cy.get('.Profile_more__s5CiB').click()
 
@@ -118,7 +114,7 @@ describe('Logout test', () => {
 
         cy.get('.logoutOption').click()
 
-        cy.wait('@loginRequest').then((interception) => {
+        cy.wait('@logoutRequest').then((interception) => {
             expect(interception.response.statusCode).to.eq(200)
             cy.wrap(interception.response.body).its('Msg').should('eq', 'cookie cleared');
         })
@@ -126,7 +122,5 @@ describe('Logout test', () => {
         cy.url().should('eq', 'https://password-manager-frontend-ten.vercel.app/login')
 
     })
-
-
 
 })
